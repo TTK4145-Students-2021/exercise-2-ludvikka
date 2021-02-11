f50d6881a9c112b309687b4ffc6cf31aba2a409f
ludvikka <ludkasbo@gmail.com>
2021-02-11 10:42:17 +0100

    finished with the C programming

--- 3 - bounded buffer/c/main.c
+++ 3 - bounded buffer/c/main.c
@@ -23,8 +23,8 @@ struct BoundedBuffer* buf_new(int size){
     
     pthread_mutex_init(&buf->mtx, NULL);
     // TODO: initialize semaphores
-    //sem_init(&buf->full,  0, /*starting value?*/);
-	//sem_init(&buf->empty, 0, /*starting value?*/);
+    sem_init(&buf->full,  0, 0);
+	sem_init(&buf->empty, 0, 5);
     
     return buf;    
 }
@@ -42,19 +42,23 @@ void buf_destroy(struct BoundedBuffer* buf){
 
 void buf_push(struct BoundedBuffer* buf, int val){    
     // TODO: wait for there to be room in the buffer
+    sem_wait(&buf->empty);
     // TODO: make sure there is no concurrent access to the buffer internals
-    
+    pthread_mutex_lock(&buf->mtx);
     rb_push(buf->buf, val);
+    pthread_mutex_unlock(&buf->mtx);
     
-    
-    // TODO: signal that there are new elements in the buffer    
+    // TODO: signal that there are new elements in the buffer 
+    sem_post(&buf->full);   
 }
 
 int buf_pop(struct BoundedBuffer* buf){
     // TODO: same, but different?
-    
+    sem_post(&buf->empty);
+    pthread_mutex_lock(&buf->mtx);
     int val = rb_pop(buf->buf);    
-    
+    pthread_mutex_unlock(&buf->mtx);
+    sem_wait(&buf->full); 
     return val;
 }
 
